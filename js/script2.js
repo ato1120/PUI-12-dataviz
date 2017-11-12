fde315f1e771ccbd0a9f9691af00c77d225053d0
Alexandra To <alexandra.akiye.to@gmail.com>
2017-11-12 10:09:00 -0800

    annotations

---
+++ js/script2.js
@@ -0,0 +1,11 @@
+//----- Part 2
+
+var MHCIday = [
+  { item: 'UCRE',  hours: 6 },
+  { item: 'PUI',  hours: 3 },
+  { item: 'Classes for Fun', hours: 2 },
+  { item: 'Bond with Other Students', hours: 4 },
+  { item: 'sleep', hours: 8},
+  { item: 'misc', hours: 1}
+];
+
